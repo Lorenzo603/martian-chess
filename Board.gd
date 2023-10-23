0e6a6cabdfb660f09f0d59d5adc6ae51f90d4e8d
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2023-10-24 00:25:23 +0100

    adding skeleton of piece movement rules

--- Board.gd
+++ Board.gd
@@ -1,7 +1,71 @@
 extends Node
 
-var board_state
+const MAX_PLAYERS = 2
 
-func is_move_valid():
+var player_turn = 1
 
-	return true
+var board_state = [
+	["B", "B", "M", ""],
+	["B", "M", "S", ""],
+	["M", "S", "S", ""],
+	["", "", "", ""],
+	
+	["", "", "", ""],
+	["", "S", "S", "M"],
+	["", "S", "M", "B"],
+	["", "M", "B", "B"],
+]
+
+
+func is_move_valid(starting_tile_x, starting_tile_y, destination_tile_x, destination_tile_y):
+	print_debug(str(starting_tile_x) + "," + str(starting_tile_y) + "[" + board_state[starting_tile_x][starting_tile_y] + "]" +
+		" --> " + str(destination_tile_x) + "," + str(destination_tile_y) + "[" + board_state[destination_tile_x][destination_tile_y] + "]")
+	
+	# cannot move piece to same position
+	if starting_tile_x == destination_tile_x and starting_tile_y == destination_tile_y:
+		return false
+	
+	# cannot capture own piece
+	# TODO: Exception for when promoting
+	if (destination_tile_x > 3 and player_turn == 1 and board_state[destination_tile_x][destination_tile_y] != "") \
+		or (destination_tile_x <= 3 and player_turn == 2 and board_state[destination_tile_x][destination_tile_y] != ""):
+		return false
+	
+	var current_piece_type = board_state[starting_tile_x][starting_tile_y]
+	var is_movement_valid = _is_piece_movement_valid(current_piece_type, starting_tile_x, starting_tile_y, destination_tile_x, destination_tile_y)
+	print_debug("is_movement_valid: " + str(is_movement_valid))
+	if is_movement_valid:
+		board_state[starting_tile_x][starting_tile_y] = ""
+		board_state[destination_tile_x][destination_tile_y] = current_piece_type
+	return is_movement_valid
+	
+func _is_piece_movement_valid(current_piece_type, starting_tile_x, starting_tile_y, destination_tile_x, destination_tile_y):
+	match current_piece_type:
+		"S":
+			return (destination_tile_x == starting_tile_x + 1 and destination_tile_y == starting_tile_y + 1) \
+				or (destination_tile_x == starting_tile_x - 1 and destination_tile_y == starting_tile_y - 1) \
+				or (destination_tile_x == starting_tile_x + 1 and destination_tile_y == starting_tile_y - 1) \
+				or (destination_tile_x == starting_tile_x - 1 and destination_tile_y == starting_tile_y + 1)
+		"M":
+			return ((destination_tile_x == starting_tile_x + 1 or (destination_tile_x == starting_tile_x + 2 and board_state[starting_tile_x + 1][starting_tile_y] == "")) and destination_tile_y == starting_tile_y) \
+				or ((destination_tile_x == starting_tile_x - 1 or (destination_tile_x == starting_tile_x - 2 and board_state[starting_tile_x - 1][starting_tile_y] == "")) and destination_tile_y == starting_tile_y) \
+				or ((destination_tile_y == starting_tile_y + 1 or (destination_tile_y == starting_tile_y + 2 and board_state[starting_tile_x][starting_tile_y + 1] == "")) and destination_tile_x == starting_tile_x) \
+				or ((destination_tile_y == starting_tile_y - 1 or (destination_tile_y == starting_tile_y - 2 and board_state[starting_tile_x][starting_tile_y - 1] == "")) and destination_tile_x == starting_tile_x)
+		"B":
+			if abs(starting_tile_x - destination_tile_x) == abs(starting_tile_y - destination_tile_y):
+				for i in range(min(starting_tile_x, destination_tile_x) + 1, max(starting_tile_x, destination_tile_x)):
+					for j in range(min(starting_tile_y, destination_tile_y) + 1, max(starting_tile_y, destination_tile_y)):
+						if board_state[i][j] != "":
+							return false
+			if starting_tile_x == destination_tile_x:
+				for i in range(min(starting_tile_y, destination_tile_y) + 1, max(starting_tile_y, destination_tile_y)):
+					if board_state[starting_tile_x][i] != "":
+						return false
+			if starting_tile_y == destination_tile_y:
+				for i in range(min(starting_tile_x, destination_tile_x) + 1, max(starting_tile_x, destination_tile_x)):
+					if board_state[i][starting_tile_y] != "":
+						return false
+			
+			return true
+
+	return false
--- PieceBig.gd
+++ PieceBig.gd
@@ -2,11 +2,13 @@ extends Sprite2D
 
 @onready var board = get_node("../Board")
 
-var canDrag = false
+var is_dragging = false
 var starting_position = self.global_position 
+@export var starting_tile_x = 0
+@export var starting_tile_y = 0
 
 func _process(delta):
-	if canDrag:
+	if is_dragging:
 		#$".".global_position = lerp($".".global_position, get_global_mouse_position(), 10 * delta)
 		self.global_position = get_global_mouse_position()
 
@@ -14,10 +16,16 @@ func _process(delta):
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			canDrag = true
+			is_dragging = true
 			starting_position = self.global_position
+			# TODO: dragging sprite should always be on the top layer
+			# TODO: make pieces not draggable when not proper player turn
+			
 		elif event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
-			canDrag = false
+			if not is_dragging: # avoids triggering release on pieces in the destination tile
+				return
+				
+			is_dragging = false
 			var has_moved_on_tile = $DragOverlapArea2D.has_overlapping_areas()
 			if not has_moved_on_tile:
 				self.global_position = starting_position
@@ -25,16 +33,22 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 				
 			var overlapping_tile_area = $DragOverlapArea2D.get_overlapping_areas()[0]
 			var overlapping_tile = overlapping_tile_area.get_parent()
-			print_debug(str(overlapping_tile.get_meta("TileCoordX")) + "," + str(overlapping_tile.get_meta("TileCoordY")))
 			
-			var is_move_valid = board.is_move_valid()
+			var destination_tile_x = overlapping_tile.get_meta("TileCoordX")
+			var destination_tile_y = overlapping_tile.get_meta("TileCoordY")
+			
+			
+			var is_move_valid = board.is_move_valid(starting_tile_x, starting_tile_y, 
+				destination_tile_x, destination_tile_y
+			)
 			if not is_move_valid:
 				self.global_position = starting_position
 				return
 				
-			# TODO update board state	
 			var clamp_position = overlapping_tile.get_node("PieceClampPosition").global_position
 			self.global_position = clamp_position
+			self.starting_tile_x = destination_tile_x
+			self.starting_tile_y = destination_tile_y
 			
 			
 
