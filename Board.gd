e7b30485da6b0832b859a115342297610ac325a7
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2023-10-30 00:11:17 +0000

    added random move AI. fixed Big piece movement validation.

--- Board.gd
+++ Board.gd
@@ -6,6 +6,7 @@ signal game_ended
 const MAX_PLAYERS = 2
 
 var player_turn = 1
+var game_over = false
 
 # row index is player index (player_turn - 1)
 var captured_pieces = [
@@ -46,28 +47,12 @@ func is_piece_draggable(starting_tile_x, starting_tile_y):
 
 func is_move_valid(starting_tile_x, starting_tile_y, destination_tile_x, destination_tile_y):
 	print_debug(str(starting_tile_x) + "," + str(starting_tile_y) + "[" + board_state[starting_tile_x][starting_tile_y] + "]" +
-		" --> " + str(destination_tile_x) + "," + str(destination_tile_y) + "[" + board_state[destination_tile_x][destination_tile_y] + "]")
-	
-	# cannot move piece to same position
-	if starting_tile_x == destination_tile_x and starting_tile_y == destination_tile_y:
-		return [false, false, ""]
-	
-	# cannot capture own piece
-	var promotion_piece = _get_promotion_piece(starting_tile_x, starting_tile_y, destination_tile_x, destination_tile_y)
-	if promotion_piece == "" \
-		and (
-			(destination_tile_x > 3 and player_turn == 1 and board_state[destination_tile_x][destination_tile_y] != "") \
-			or (destination_tile_x <= 3 and player_turn == 2 and board_state[destination_tile_x][destination_tile_y] != "")
-		):
-		return [false, false, ""]
-		
-	# cannot "reject" move
-	if previous_starting_tile_x == destination_tile_x and previous_starting_tile_y == destination_tile_y \
-		and previous_destination_tile_x == starting_tile_x and previous_destination_tile_y == starting_tile_y:
-		return [false, false, ""]
+		" --> " + str(destination_tile_x) + "," + str(destination_tile_y) + "[" + board_state[destination_tile_x][destination_tile_y] + "]")	
 	
 	var current_piece_type = board_state[starting_tile_x][starting_tile_y]
-	var is_movement_valid = _is_piece_movement_valid(current_piece_type, starting_tile_x, starting_tile_y, destination_tile_x, destination_tile_y)
+	var is_movement_valid_result = is_piece_movement_valid(current_piece_type, starting_tile_x, starting_tile_y, destination_tile_x, destination_tile_y)
+	var is_movement_valid = is_movement_valid_result[0]
+	var promotion_piece = is_movement_valid_result[1]
 	
 	var has_captured = false
 	if is_movement_valid:
@@ -83,6 +68,7 @@ func is_move_valid(starting_tile_x, starting_tile_y, destination_tile_x, destina
 		previous_destination_tile_x = destination_tile_x
 		previous_destination_tile_y = destination_tile_y
 		if is_game_ended():
+			game_over = true
 			game_ended.emit()
 		else:
 			update_player_turn()
@@ -91,38 +77,60 @@ func is_move_valid(starting_tile_x, starting_tile_y, destination_tile_x, destina
 func _calculate_score(captured_pieces_list):
 	return captured_pieces_list.reduce(func(accum, piece_type): return accum + piece_value_map[piece_type], 0)
 	
-func _is_piece_movement_valid(current_piece_type, starting_tile_x, starting_tile_y, destination_tile_x, destination_tile_y):
+func is_piece_movement_valid(current_piece_type, starting_tile_x, starting_tile_y, destination_tile_x, destination_tile_y):
+	# cannot move piece to same position
+	if starting_tile_x == destination_tile_x and starting_tile_y == destination_tile_y:
+		return [false, ""]
+	
+	# cannot capture own piece
+	var promotion_piece = _get_promotion_piece(starting_tile_x, starting_tile_y, destination_tile_x, destination_tile_y)
+	if promotion_piece == "" \
+		and (
+			(destination_tile_x > 3 and player_turn == 1 and board_state[destination_tile_x][destination_tile_y] != "") \
+			or (destination_tile_x <= 3 and player_turn == 2 and board_state[destination_tile_x][destination_tile_y] != "")
+		):
+		return [false, ""]
+	
+	# cannot "reject" move
+	if previous_starting_tile_x == destination_tile_x and previous_starting_tile_y == destination_tile_y \
+		and previous_destination_tile_x == starting_tile_x and previous_destination_tile_y == starting_tile_y:
+		return [false, ""]
+	
 	match current_piece_type:
 		"S":
-			return (destination_tile_x == starting_tile_x + 1 and destination_tile_y == starting_tile_y + 1) \
+			return [(destination_tile_x == starting_tile_x + 1 and destination_tile_y == starting_tile_y + 1) \
 				or (destination_tile_x == starting_tile_x - 1 and destination_tile_y == starting_tile_y - 1) \
 				or (destination_tile_x == starting_tile_x + 1 and destination_tile_y == starting_tile_y - 1) \
-				or (destination_tile_x == starting_tile_x - 1 and destination_tile_y == starting_tile_y + 1)
+				or (destination_tile_x == starting_tile_x - 1 and destination_tile_y == starting_tile_y + 1), promotion_piece]
 		"M":
-			return ((destination_tile_x == starting_tile_x + 1 or (destination_tile_x == starting_tile_x + 2 and board_state[starting_tile_x + 1][starting_tile_y] == "")) and destination_tile_y == starting_tile_y) \
+			return [((destination_tile_x == starting_tile_x + 1 or (destination_tile_x == starting_tile_x + 2 and board_state[starting_tile_x + 1][starting_tile_y] == "")) and destination_tile_y == starting_tile_y) \
 				or ((destination_tile_x == starting_tile_x - 1 or (destination_tile_x == starting_tile_x - 2 and board_state[starting_tile_x - 1][starting_tile_y] == "")) and destination_tile_y == starting_tile_y) \
 				or ((destination_tile_y == starting_tile_y + 1 or (destination_tile_y == starting_tile_y + 2 and board_state[starting_tile_x][starting_tile_y + 1] == "")) and destination_tile_x == starting_tile_x) \
-				or ((destination_tile_y == starting_tile_y - 1 or (destination_tile_y == starting_tile_y - 2 and board_state[starting_tile_x][starting_tile_y - 1] == "")) and destination_tile_x == starting_tile_x)
+				or ((destination_tile_y == starting_tile_y - 1 or (destination_tile_y == starting_tile_y - 2 and board_state[starting_tile_x][starting_tile_y - 1] == "")) and destination_tile_x == starting_tile_x), promotion_piece]
 		"B":
 			if abs(starting_tile_x - destination_tile_x) == abs(starting_tile_y - destination_tile_y):
 				for num_gaps in range(1, abs(starting_tile_x - destination_tile_x)):
 					var i = starting_tile_x + num_gaps * (1 if destination_tile_x > starting_tile_x else -1)
 					var j = starting_tile_y + num_gaps * (1 if destination_tile_y > starting_tile_y else -1)
 					if board_state[i][j] != "":
-						return false
+						return [false, promotion_piece]
+				return [true, promotion_piece]
 
 			if starting_tile_x == destination_tile_x:
 				for i in range(min(starting_tile_y, destination_tile_y) + 1, max(starting_tile_y, destination_tile_y)):
 					if board_state[starting_tile_x][i] != "":
-						return false
+						return [false, promotion_piece]
+				return [true, promotion_piece]
+			
 			if starting_tile_y == destination_tile_y:
 				for i in range(min(starting_tile_x, destination_tile_x) + 1, max(starting_tile_x, destination_tile_x)):
 					if board_state[i][starting_tile_y] != "":
-						return false
+						return [false, promotion_piece]
+				return [true, promotion_piece]
 			
-			return true
+			return [false, promotion_piece]
 
-	return false
+	return [false, promotion_piece]
 
 # If you have no Queens, you can create one by moving a Drone into a Pawn’s space (or vice versa)
 # and merging them. Similarly, if you control no Drones, you can make one by merging two of your Pawns.
--- GameController.gd
+++ GameController.gd
@@ -8,6 +8,8 @@ enum GameMode {TWO_PLAYER, RANDOM_AI, SMART_AI}
 
 @onready var board = get_node("../Board")
 
+# TODO: player goes second
+
 func _ready():
 	SignalBus.piece_moved.connect(_on_piece_moved)
 	SignalBus.end_turn.connect(_on_end_turn)
@@ -30,6 +32,9 @@ func _on_piece_moved(moved_piece, move_result, destination_tile_x, destination_t
 	moved_piece.starting_tile_y = destination_tile_y
 	
 func _on_end_turn():
+	if board.game_over:
+		return
+		
 	match game_mode:
 		GameMode.TWO_PLAYER:
 			return
@@ -40,22 +45,132 @@ func _on_end_turn():
 			return
 	
 func ai_move_random():
-	await get_tree().create_timer(2.0).timeout
+	await get_tree().create_timer(1.0).timeout
 	
-	var starting_tile_x = 2
-	var starting_tile_y = 0
-	var destination_tile_x = 3
-	var destination_tile_y = 0
+	var next_move = _get_random_move()
+	var moved_piece = _get_tile_by_coord(next_move["starting_tile_x"], next_move["starting_tile_y"]).piece
+	var destination_tile = _get_tile_by_coord(next_move["destination_tile_x"], next_move["destination_tile_y"])
 	
-	var moved_piece = _get_tile_by_coord(starting_tile_x, starting_tile_y).piece
-	var destination_tile = _get_tile_by_coord(destination_tile_x, destination_tile_y)
-	
-	var move_result = board.is_move_valid(starting_tile_x, starting_tile_y, 
-		destination_tile_x, destination_tile_y
+	var move_result = board.is_move_valid(next_move["starting_tile_x"], next_move["starting_tile_y"], 
+		next_move["destination_tile_x"], next_move["destination_tile_y"]
 	)
-	SignalBus.piece_moved.emit(moved_piece, move_result, destination_tile_x, destination_tile_y, 
+	SignalBus.piece_moved.emit(moved_piece, move_result, 
+		next_move["destination_tile_x"], next_move["destination_tile_y"], 
 		destination_tile)
 
+func _get_random_move():
+	var valid_moves = _get_valid_moves()
+	var random_move = valid_moves[randi() % len(valid_moves)]
+	return {
+		"starting_tile_x": random_move["starting_tile_x"],
+		"starting_tile_y": random_move["starting_tile_y"],
+		"destination_tile_x": random_move["destination_tile_x"],
+		"destination_tile_y": random_move["destination_tile_y"]
+	}
+
+func _get_valid_moves():
+	var valid_moves = []
+	var available_pieces = []
+	# only looks at top side of board
+	var board_state = board.board_state
+	for i in range(0, 4):
+			for j in range(0, 4):
+				if board_state[i][j] != "":
+					available_pieces.append(
+						{
+							"sx": i,
+							"sy": j,
+							"piece_type": board_state[i][j]
+					})
+	for p in available_pieces:
+		valid_moves.append_array(_get_valid_moves_for_piece_coord(p))
+		
+	return valid_moves
+
+func _get_valid_moves_for_piece_coord(piece_coord):
+	var board_state = board.board_state
+	var valid_moves = []
+	match piece_coord["piece_type"]:
+		"S": 
+			for i in [-1, 1]:
+				for j in [-1, 1]:
+					var dx = piece_coord["sx"] + i
+					var dy = piece_coord["sy"] + j
+					if 0 <= dx and dx <= 7 and 0 <= dy and dy <= 3 \
+						and board.is_piece_movement_valid("S", piece_coord["sx"], piece_coord["sy"], 
+								dx, dy)[0]:
+						valid_moves.append({
+							"starting_tile_x": piece_coord["sx"],
+							"starting_tile_y": piece_coord["sy"],
+							"destination_tile_x": dx,
+							"destination_tile_y": dy
+						})
+		"M": 
+			for i in [-2, -1, 1, 2]:
+				var dx = piece_coord["sx"] + i
+				var dy = piece_coord["sy"]
+				if 0 <= dx and dx <= 7 and 0 <= dy and dy <= 3 \
+					and board.is_piece_movement_valid("M", piece_coord["sx"], piece_coord["sy"], 
+							dx, dy)[0]:
+					valid_moves.append({
+						"starting_tile_x": piece_coord["sx"],
+						"starting_tile_y": piece_coord["sy"],
+						"destination_tile_x": dx,
+						"destination_tile_y": dy
+					})
+			for j in [-2, -1, 1, 2]:
+				var dx = piece_coord["sx"]
+				var dy = piece_coord["sy"] + j
+				if 0 <= dx and dx <= 7 and 0 <= dy and dy <= 3 \
+					and board.is_piece_movement_valid("M", piece_coord["sx"], piece_coord["sy"], 
+							dx, dy)[0]:
+					valid_moves.append({
+						"starting_tile_x": piece_coord["sx"],
+						"starting_tile_y": piece_coord["sy"],
+						"destination_tile_x": dx,
+						"destination_tile_y": dy
+					})
+						
+		"B": 
+			"""
+			for i in range(0, 8):
+				var dx = i
+				var dy = piece_coord["sy"]
+				if dx != piece_coord["sx"] \
+					and board.is_piece_movement_valid("B", piece_coord["sx"], piece_coord["sy"], 
+							dx, dy)[0]:
+					valid_moves.append({
+						"starting_tile_x": piece_coord["sx"],
+						"starting_tile_y": piece_coord["sy"],
+						"destination_tile_x": dx,
+						"destination_tile_y": dy
+					})
+			for j in range(0, 4):
+				var dx = piece_coord["sx"]
+				var dy = j
+				if dy != piece_coord["sy"] \
+					and board.is_piece_movement_valid("B", piece_coord["sx"], piece_coord["sy"], 
+							dx, dy)[0]:
+					valid_moves.append({
+						"starting_tile_x": piece_coord["sx"],
+						"starting_tile_y": piece_coord["sy"],
+						"destination_tile_x": dx,
+						"destination_tile_y": dy
+					})
+			"""	
+			for i in range(0, 8):
+				for j in range(0, 4):
+					if not (i == piece_coord["sx"] and j == piece_coord["sy"]) \
+						and board.is_piece_movement_valid("B", piece_coord["sx"], piece_coord["sy"], 
+							i, j)[0]:
+						valid_moves.append({
+							"starting_tile_x": piece_coord["sx"],
+							"starting_tile_y": piece_coord["sy"],
+							"destination_tile_x": i,
+							"destination_tile_y": j
+						})
+	return valid_moves
+	
 func _get_tile_by_coord(x, y):
 	for t in board.get_children():
 		if t.get_meta("TileCoordX") == x and t.get_meta("TileCoordY") == y:
